3755cd2b1adedb9415fe2f466671054f9ba173bd
alessandrorico99 <alessandrorico99@gmail.com>
2022-08-02 22:21:37 +0200

    Gengis capo Co-authored-by: Oran <OranBar@users.noreply.github.com>

--- ProvaSplit.js
+++ ProvaSplit.js
@@ -1,4 +1,5 @@
-var dic = {}
+// var dic = {}
+
 const input =
     document.querySelector('input[type="file"]')
 
@@ -7,10 +8,72 @@ input.addEventListener('change', function (e) {
     const reader = new FileReader()
 
     reader.onload = function () {
-        const lines = reader.result.split('\n').map(function (line) {
-           return line.split(',').map((x,idx)=>({[idx]:x}))
-        })
+        const lines = reader.result.replace("\r", "").split('\n')
         console.log(lines)
+
+
+        lines.forEach(linea => {
+
+            var cella = linea.split(",")
+            var nomeAgo = cella[0]
+            var angolo = cella[1]
+            var dimensione = cella[2]
+            DisegnaAgo(nomeAgo, angolo, dimensione)
+        }
+        );
     }
     reader.readAsText(input.files[0])
 }, false)
+
+function DisegnaAgo(nomeAgo, angolo, dim) {
+    var dividendo
+    var moltiplicatore
+
+    var angStart
+    var angFine
+
+
+    if (angolo = 180) {
+        dividendo = 1
+        moltiplicatore = 2
+
+        angStart = 0
+        angFine = 180
+
+    }
+    else if (angolo = 135) {
+        dividendo = 3
+        moltiplicatore = 8
+
+        angStart = 0
+        angFine = 135
+    }
+
+
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 4;
+
+    var r = ((((moltiplicatore * dim) / dividendo) / 2 * Math.PI) * 3.77);
+
+    console.log(r);
+
+    r = r + ((r * 20) / 100);
+    var radius = r;
+    var x = (Math.PI / 180) * angStart
+    var y = (Math.PI / 180) * angFine
+    var startingAngle = x;
+    var endingAngle = y;
+    var counterclockwise = false;
+
+    context.arc(centerX, centerY, radius, startingAngle, endingAngle, counterclockwise);
+
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+    context.stroke();
+    //sql
+
+}
