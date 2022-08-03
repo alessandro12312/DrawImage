869b9802bdcd31d0ac32c0c2deec819dceaac240
AlessandroRico <alessandrorico99@gmail.com>
2022-08-03 09:19:54 +0200

    Codice Connessione mssql

--- ProvaSplit.js
+++ ProvaSplit.js
@@ -38,7 +38,7 @@ function DisegnaAgo(nomeAgo, angolo, dim) {
         moltiplicatore = 2
 
         angStart = 0
-        angFine = 180
+        angFine = angolo
 
     }
     else if (angolo = 135) {
@@ -46,7 +46,14 @@ function DisegnaAgo(nomeAgo, angolo, dim) {
         moltiplicatore = 8
 
         angStart = 0
-        angFine = 135
+        angFine = angolo
+    }
+    else if (angolo = 225) {
+        dividendo = 5
+        moltiplicatore = 8
+
+        angStart = 0
+        angFine = angolo
     }
 
 
@@ -75,6 +82,34 @@ function DisegnaAgo(nomeAgo, angolo, dim) {
     context.lineWidth = 1;
     context.strokeStyle = "black";
     context.stroke();
-    //sql
+
+
+    // const sql = require('mssql')
+    // const sqlConfig = {
+    //     user: process.env.DB_USER,
+    //     password: process.env.DB_PWD,
+    //     database: process.env.DB_NAME,
+    //     server: 'localhost',
+    //     pool: {
+    //         max: 10,
+    //         min: 0,
+    //         idleTimeoutMillis: 30000
+    //     },
+    //     options: {
+    //         encrypt: true, // for azure
+    //         trustServerCertificate: false // change to true for local dev / self-signed certs
+    //     }
+    // }
+
+    // async () => {
+    //     try {
+    //         // make sure that any items are correctly URL encoded in the connection string
+    //         await sql.connect(sqlConfig)
+    //         const result = await sql.query`select * from mytable where id = ${value}`
+    //         console.dir(result)
+    //     } catch (err) {
+    //         // ... error checks
+    //     }
+    // }
 
 }
