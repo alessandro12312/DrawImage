1ea5bba7205c0ef524a0953aa28f0fe1c847c554
AlessandroRico <alessandrorico99@gmail.com>
2022-08-03 14:01:02 +0200

    ProvaConnessione MMSQL

--- ProvaSplit.js
+++ ProvaSplit.js
@@ -1,5 +1,4 @@
 // var dic = {}
-
 const input =
     document.querySelector('input[type="file"]')
 
@@ -81,35 +80,22 @@ function DisegnaAgo(nomeAgo, angolo, dim) {
 
     context.lineWidth = 1;
     context.strokeStyle = "black";
-    context.stroke();
-
-
-    // const sql = require('mssql')
-    // const sqlConfig = {
-    //     user: process.env.DB_USER,
-    //     password: process.env.DB_PWD,
-    //     database: process.env.DB_NAME,
-    //     server: 'localhost',
-    //     pool: {
-    //         max: 10,
-    //         min: 0,
-    //         idleTimeoutMillis: 30000
-    //     },
-    //     options: {
-    //         encrypt: true, // for azure
-    //         trustServerCertificate: false // change to true for local dev / self-signed certs
-    //     }
-    // }
-
-    // async () => {
-    //     try {
-    //         // make sure that any items are correctly URL encoded in the connection string
-    //         await sql.connect(sqlConfig)
-    //         const result = await sql.query`select * from mytable where id = ${value}`
-    //         console.dir(result)
-    //     } catch (err) {
-    //         // ... error checks
-    //     }
-    // }
-
+    context.stroke();  
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
+var mssql=request('mssql')
+var config = {
+    "user": "TEST", //default is sa
+    "password": "tiger",
+    "server": "192.168.172.18", // for local machine
+    "options": {
+        "encrypt": true
+    }
+  }
+
+connect(config, err => { 
+if(err){
+    throw err ;
+}
+console.log("Connection Successful !");
+})
