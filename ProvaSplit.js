a2b628e075c7b8b6aa2c3b546b30431a646e12bd
AlessandroRico <rico.1895511@studenti.uniroma1.it>
2022-08-10 09:32:16 +0200

    Ago Retto da completare

--- ProvaSplit.js
+++ ProvaSplit.js
@@ -12,18 +12,18 @@ input.addEventListener('change', function (e) {
     const reader = new FileReader()
 
     reader.onload = function () {
-        const lines = reader.result.replace("\r",'').split('\n')
+        const lines = reader.result.replace("\r", '').split('\n')
         console.log(lines)
 
 
-        lines.forEach(linea =>{
+        lines.forEach(linea => {
 
             var cella = linea.split(",")
             var nomeAgo = cella[0]
             var angolo = parseFloat(cella[1])
             var dimensione = parseFloat(cella[2])
             var tipo = cella[3]
-            console.log(nomeAgo,angolo,dimensione)
+            console.log(nomeAgo, angolo, dimensione)
             DisegnaAgo(nomeAgo, angolo, dimensione)
         }
         );
@@ -31,54 +31,64 @@ input.addEventListener('change', function (e) {
     reader.readAsText(input.files[0])
 }, false)
 
-function DisegnaAgo(nomeAgo,angolo,dim) {
+function DisegnaAgo(nomeAgo, angolo, dim) {
+    if (angolo != 0) {
+        if (angolo == 135) {
 
-    if(angolo == 135){
+            var dividendo = 3
+            var moltiplicatore = 8
 
-        var dividendo = 3
-        var moltiplicatore = 8
+            var angStart = 0
+            var angFine = angolo
+        } else if (angolo == 180) {
+            dividendo = 1
+            moltiplicatore = 2
 
-        var angStart = 0
-        var angFine = angolo
-    } else if (angolo == 180) {
-        dividendo = 1
-        moltiplicatore = 2
+            angStart = 0
+            angFine = angolo
+        } else if (angolo == 225) {
+            dividendo = 5
+            moltiplicatore = 8
 
-        angStart = 0
-        angFine = angolo
-    }else if (angolo == 225) {
-        dividendo = 5
-        moltiplicatore = 8
+            angStart = 0
+            angFine = angolo
+        }
 
-        angStart = 0
-        angFine = angolo
+        var centerX = canvas.width / 2;
+        var centerY = canvas.height / 4;
+
+        var r = ((((moltiplicatore * dim) / dividendo) / 2 * Math.PI) * 3.77);
+
+        console.log(r);
+
+
+        r = r + ((r * 20) / 100);
+        var radius = r;
+        var x = (Math.PI / 180) * angStart
+        var y = (Math.PI / 180) * angFine
+        var startingAngle = x;
+        var endingAngle = y;
+        var counterclockwise = false;
+
+        context.arc(centerX, centerY, radius, startingAngle, endingAngle, counterclockwise);
+
+        context.lineWidth = 1;
+        context.strokeStyle = "black";
+        context.stroke();
+    }
+    else {
+        context.lineWidth=1;
+        var centerX = canvas.width / 2;
+        var centerY = canvas.height / 4;
+
+        context.moveTo(centerX,centerY);
+        context.lineTo(130, 130);
+        context.stroke();
     };
-
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 4;
-
-    var r = ((((moltiplicatore * dim) / dividendo) / 2 * Math.PI) * 3.77);
-
-    console.log(r);
-
-
-    r = r + ((r * 20) / 100);
-    var radius = r;
-    var x = (Math.PI / 180) * angStart
-    var y = (Math.PI / 180) * angFine
-    var startingAngle = x;
-    var endingAngle = y;
-    var counterclockwise = false;
-
-    context.arc(centerX, centerY, radius, startingAngle, endingAngle, counterclockwise);
-
-    context.lineWidth = 1;
-    context.strokeStyle = "black";
-    context.stroke();
-    function ScaricaFile(){
+    function ScaricaFile() {
         var dataUrl = canvas.toDataURL("image/png");
         var link = document.createElement('a');
-        link.download = nomeAgo+".png";
+        link.download = nomeAgo + ".png";
         link.href = dataUrl.replace("image/png", "image/octet-stream");
         console.log(link);
         link.click();
@@ -86,12 +96,24 @@ function DisegnaAgo(nomeAgo,angolo,dim) {
     ScaricaFile()
     document.getElementById('cancella').click();
 
-}
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
+    let post = JSON.stringify(nomeAgo)
+ 
+    const url = "localhost:5000"
+    let xhr = new XMLHttpRequest()
+ 
+    xhr.open('POST', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    xhr.send(post);
+ 
+    xhr.onload = function () {
+        if(xhr.status === 201) {
+            console.log("Post successfully created!") 
+        }
+    }
 
-function clean(){
+}
+
+function clean() {
     context.save();
     context.setTransform(1, 0, 0, 1, 0, 0);
     context.clearRect(0, 0, canvas.width, canvas.height);
